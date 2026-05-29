233db7d185e6514507f4aaab9b2351328e20cd3e
haslansalgado <haslansalgado@hotmail.com>
2026-05-29 13:25:01 -0300

    Primeiro commit

---
+++ skillmach.js
@@ -0,0 +1,15 @@
+console.log("SkillMatch JS iniciado!");
+
+const candidato = {
+  nome: "Luiz Felipe",
+  area: "Front-End",
+  habilidades: [
+    "javaScript",
+    "GitHub",
+    "Lógica de Programação",
+    "kanban",
+    "Python"
+  ],
+  experienciaMeses: 12
+
+};
