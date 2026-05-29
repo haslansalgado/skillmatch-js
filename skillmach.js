58918ae0e859c097665fc0439d6a5e0478e6f487
haslansalgado <haslansalgado@hotmail.com>
2026-05-29 14:20:35 -0300

    Adiciona lista de vagas e testes com console.log

--- skillmach.js
+++ skillmach.js
@@ -13,3 +13,50 @@ const candidato = {
   experienciaMeses: 12
 
 };
+
+const vagas = [
+  {
+    id: 1,
+    empresa: "Tecnologia TonyStark",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: [
+      "JavaScript",
+      "GitHub",
+      "Lógica de Programação"
+    ],
+
+    salario: 2800,
+    modalidade: "Remoto"
+
+  },
+
+  {
+    id: 2,
+    empresa: "Programadores.com",
+    cargo: "Estágio Front-End",
+    requisitos: [
+      "JavaScript",
+      "Kanban",
+      "GitHub"
+    ],
+
+    salario: 1800,
+    modalidade: "híbrido"
+  },
+  {
+    id: 3,
+    empresa: "WebDesenvolvedores",
+    cargo: "Programador JavaScript Júnior",
+    requisitos: [
+      "JavaScript",
+      "Arrays",
+      "Objetos",
+      "Funções"
+    ],
+
+    salario: 3000,
+    modalidade: "presencial"
+  }
+];
+
+console.log(vagas);
